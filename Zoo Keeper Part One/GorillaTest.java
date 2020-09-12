d462559cf609007320a987c185cf6cc50635d090
ozairkwaseem <63318347+ozairkwaseem@users.noreply.github.com>
2020-09-12 15:20:47 -0400

    Add files via upload

---
+++ Zoo Keeper Part One/GorillaTest.java
@@ -0,0 +1,13 @@
+public class GorillaTest {
+    public static void main(String[] args) {
+        Gorilla g = new Gorilla();
+        g.setEnergyLevel(100);
+        g.climb();
+        g.throwSomething();
+        g.getEnergyLevel();
+        System.out.println(g.energyLevel);
+
+    }
+
+
+}
---
+++ Zoo Keeper Part One/Mammal.java
@@ -0,0 +1,19 @@
+public class Mammal{
+
+public double energyLevel;
+
+    public double displayEnergy(){
+
+        return energyLevel;
+    }
+
+    public void setEnergyLevel(double energyLevel){
+        this.energyLevel= energyLevel;
+    }
+    public double getEnergyLevel(){
+        return energyLevel;
+    }
+
+
+
+}
